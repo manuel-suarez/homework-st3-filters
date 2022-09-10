4092aca723b59b344a0a2a3079389a4474b2cf5d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-10 13:02:54 -0500

    Load sample from file

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 
 int main()
 {
-    std::string image_path = samples::findFile("starry_night.jpg");
+    std::string image_path = "starry_night.jpg";
     Mat img = imread(image_path, IMREAD_COLOR);
 
     if (img.empty()) {
