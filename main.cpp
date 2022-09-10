1006474c63651cbfd05de63c4cad94808718852d
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-09 21:19:18 -0500

    Main file

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+    std::string image_path = samples::findFile("starry_night.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+
+    if (img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+    imshow("Display window", img);
+    int k = waitKey(0);
+
+    if (k == 's')
+    {
+        imwrite("starry_night.png", img);
+    }
+
+    return 0;
+}
