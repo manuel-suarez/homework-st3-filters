86120a4687b314810bb2d3bfe687a48b12e42016
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-10 12:55:49 -0500

    Remove gui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 
 #include <iostream>
 
@@ -16,7 +15,6 @@ int main()
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
-    imshow("Display window", img);
     int k = waitKey(0);
 
     if (k == 's')
